7557294c673c8d28ff9a2174b0f2f3cdc7e81831
swasthikG <129170239+swasthikG@users.noreply.github.com>
2025-02-18 10:29:30 +0000

    Separated out the script file

--- src/script.js
+++ src/script.js
@@ -1,12 +1,47 @@
-buttonclicked = false;
 
-function Clicked(){
-    this.buttonclicked = true
-}
+document.addEventListener('DOMContentLoaded', function () {
 
-document.addEventListener(buttonclicked, function(){
-    console.log("The Button is clicked");
-    
+    const buttons = document.getElementsByClassName('btn');
+    const calcDisplay = document.getElementById('calc-display');
+    let currentValue = "";
+
+    function undo() {
+        let trimValue = '';
+        if (currentValue.length >= 1) {
+            trimValue = currentValue.slice(0, -1);
+            currentValue = trimValue;
+            calcDisplay.value = trimValue;
+        }
+    }
+
+    function evaluateResult() {
+        const currentvalue = currentValue;
+        console.log('CurrentValue: ', currentvalue);
+        const ConvertedValue = currentvalue.replace('×', '*').replace('÷', '/');
+        console.log('ConvertedValue: ', ConvertedValue);
+        const result = eval(ConvertedValue);
+        calcDisplay.value = result;
+    }
+
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        button.addEventListener('click', function () {
+            const value = button.innerText;
+
+            if (value == 'AC') {
+                currentValue = "";
+                calcDisplay.value = currentValue;
+            } else if (value == "=") {
+                evaluateResult();
+            } else if (value == "↻") {
+                undo();
+            }
+            else {
+                currentValue += value;
+                calcDisplay.value = currentValue;
+            }
+        })
+    }
+
+
 })
-
-
