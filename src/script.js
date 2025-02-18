8a9cd2ffc3f984a4cec9d858db9deff54aa5802a
swasthikG <129170239+swasthikG@users.noreply.github.com>
2025-02-18 10:15:16 +0000

    Calculator Basic logic Added

---
+++ src/script.js
@@ -0,0 +1,12 @@
+buttonclicked = false;
+
+function Clicked(){
+    this.buttonclicked = true
+}
+
+document.addEventListener(buttonclicked, function(){
+    console.log("The Button is clicked");
+    
+})
+
+
